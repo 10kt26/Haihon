c866ae549d11e277663c5c26219b7eefd9c4496a
10kt26 <7203qm@gmail.com>
2023-08-30 19:21:13 +0900

    small update

--- haihon_project/lib/settings/settings.dart
+++ haihon_project/lib/settings/settings.dart
@@ -8,15 +8,21 @@ class SheetSettings {
   static const sheetname = '';
 
   //A列が1,B列が2・・・
+  //予約番号の列
   static const referenceNumberColumn=1;
+  //学生番号の列
   static const studentNumberColumn=2;
+  //名前の列
   static const nameColumn=3;
-  static const deliverColumn=22;
-  //static const receiveDoneColumn=5;
-  static const receivePersonColumn=28;
-  static const booksNumberColumn=18;
-  //static const cancelColumn=8;
-  static const remarkColumn=25;
+  //配達かの列
+  static const deliverColumn=4;
+  //受取人の列
+  static const receivePersonColumn=5;
+  //冊数の列
+  static const booksNumberColumn=6;
+  //備考の列
+  static const remarkColumn=7;
+  //受取日時の列
   static const timeColumn=29;
   //表記揺れ対策
   static const directString="直接引換";
